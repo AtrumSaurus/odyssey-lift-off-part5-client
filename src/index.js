251a5e32adafe9b7099bc71182df6e5dccf04b1e
Roberto Mapelli <rmapelli@deloitte.co.uk>
2022-07-27 20:56:05 +0100

    switch gql url to prod

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import GlobalStyles from "./styles";
 import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+const URI = Object.freeze({
+  PROD_DEFAULT: "https://server-catstronauts.herokuapp.com/",
+  PROD_HEROKU: "https://server-catstronauts-atrum.herokuapp.com/",
+  LOCAL: 'http://localhost:4000'
+})
+
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  uri: URI.PROD_HEROKU, // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
